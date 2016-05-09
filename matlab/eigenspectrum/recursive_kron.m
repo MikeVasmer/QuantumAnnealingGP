6e72f84752a339ee42bde70338402954675a3e51
Alex Morgan <axm108@gmail.com>
2016-05-09 22:03:12 +0100

    More concise implementation of recursive_kron functions.

--- matlab/eigenspectrum/recursive_kron.m
+++ matlab/eigenspectrum/recursive_kron.m
@@ -3,47 +3,29 @@ function [ out ] = recursive_kron( n, i, ins_mat, prev_mat )
     % 2x2 Identity matrix
     iden = [[1, 0] ;[ 0, 1]];
 
-    % If first call, first_call = true, else first_call = false
+    % If first call, set previous matrix to [1]
     switch nargin
     case 3
-        first_call = 1;
-    case 4
-        first_call = 0;
+        prev_mat = [1];
     end
     
     % Decrement counter
     n = n-1;
-    
-    % If first call, need to chose two 2x2 matrices
-    if first_call
-        % Decrement counter again because we are inserting two elements
-        n = n-1;
-        % If should insert matrix at end
-        if i==(n+2)
-            out = recursive_kron(n, i, ins_mat, kron(iden,ins_mat));
-        % If should inset matrix at end-1
-        elseif i==(n+1)
-            out = recursive_kron(n, i, ins_mat, kron(ins_mat,iden));
-        % If should not inset matrix
-        else
-            out = recursive_kron(n, i, ins_mat, kron(iden,iden));
-        end
-    % If not first call, need to chose one 2x2 matrices
+
+    % If should finish
+    if n==-1
+        out = prev_mat;
+    % If should call recursively
     else
-        % If should finish
-        if n==-1
-            out = prev_mat;
-        % If should call recursively
+        % If should insert matrix next
+        if i==(n+1)
+            out = recursive_kron(n, i, ins_mat, kron(ins_mat,prev_mat));
+        % If should insert identity
         else
-            % If should insert matrix next
-            if i==(n+1)
-                out = recursive_kron(n, i, ins_mat, kron(ins_mat,prev_mat));
-            % If should not inset matrix
-            else
-                out = recursive_kron(n, i, ins_mat, kron(iden,prev_mat));
-            end
+            out = recursive_kron(n, i, ins_mat, kron(iden,prev_mat));
         end
     end
+
    
 end
 
--- matlab/eigenspectrum/recursive_kron_double.m
+++ matlab/eigenspectrum/recursive_kron_double.m
@@ -8,60 +8,29 @@ function [ out ] = recursive_kron_double( n, i1, ins_mat1, i2, ins_mat2, prev_ma
     % 2x2 Identity matrix
     iden = [[1, 0] ;[ 0, 1]];
 
-    % If first call, first_call = true, else first_call = false
+    % If first call, set previous matrix to [1]
     switch nargin
     case 5
-        first_call = 1;
-    case 6
-        first_call = 0;
+        prev_mat = [1];
     end
     
     % Decrement counter
     n = n-1;
     
-    % If first call, need to chose two 2x2 matrices
-    if first_call
-        % Decrement counter again because we are inserting two elements
-        n = n-1;
-        % If 
-        if i1==(n+2) && i2==(n+1)
-            out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(ins_mat2,ins_mat1));
-        % If 
-        elseif i1==(n+1) && i2==(n+2)
-            out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(ins_mat1,ins_mat2));
-        % If 
-        elseif i1==(n+2)
-            out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(iden,ins_mat1));
-        % If 
-        elseif i1==(n+1)
-            out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(ins_mat1,iden));
-        % If 
-        elseif i2==(n+2)
-            out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(iden,ins_mat2));
-        % If 
-        elseif i2==(n+1)
-            out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(ins_mat2,iden));
-        % If should not insert matrix
-        else
-            out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(iden,iden));
-        end
-    % If not first call, need to chose one 2x2 matrices
+    % If should finish
+    if n==-1
+        out = prev_mat;
+    % If should call recursively
     else
-        % If should finish
-        if n==-1
-            out = prev_mat;
-        % If should call recursively
+        % If should insert i1 matrix next
+        if i1==(n+1)
+            out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(ins_mat1,prev_mat));
+        % If should insert i2 matrix next
+        elseif i2==(n+1)
+            out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(ins_mat2,prev_mat));
+        % If should insert identity
         else
-            % If should insert i1 matrix next
-            if i1==(n+1)
-                out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(ins_mat1,prev_mat));
-            % If should insert i2 matrix next
-            elseif i2==(n+1)
-                out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(ins_mat2,prev_mat));
-            % If should not inset matrix
-            else
-                out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(iden,prev_mat));
-            end
+            out = recursive_kron_double(n, i1, ins_mat1, i2, ins_mat2, kron(iden,prev_mat));
         end
     end
      
