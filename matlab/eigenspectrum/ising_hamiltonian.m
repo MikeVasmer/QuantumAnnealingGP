b55775ef95c7214d66bd5e7049bcd6accb425807
Alex Morgan <axm108@gmail.com>
2016-05-12 23:28:17 +0100

    Improved performance of ising_hamiltonian

--- matlab/eigenspectrum/ising_hamiltonian.m
+++ matlab/eigenspectrum/ising_hamiltonian.m
@@ -15,23 +15,23 @@ function [ out ] = ising_hamiltonian( h, Jzz, Jxx, Jzzz, Jxxx )
     jxxx_term = 0;
     % Loop though dimension 1
     for i=1:n
-        if length(h) > 1
+        if length(h) > 1 && h(i) ~= 0
             h_term = h_term + h(i)*recursive_kron( n, [i], [sigmaZ] );
         end
         % Loop though dimension 2
         for k=i+1:n
-            if length(Jzz) > 1
+            if length(Jzz) > 1 && Jzz(i,k) ~= 0
                 jzz_term = jzz_term + Jzz(i,k)*recursive_kron( n, [i,k], [sigmaZ,sigmaZ] );
             end
-            if length(Jxx) > 1
+            if length(Jxx) > 1 && Jxx(i,k) ~= 0
                 jxx_term = jxx_term + Jxx(i,k)*recursive_kron( n, [i,k], [sigmaX,sigmaX] );
             end
             % Loop though dimension 3
             for l=k+1:n
-                if length(Jzzz) > 1
+                if length(Jzzz) > 1 && Jzzz(i,k,l) ~= 0
                     jzzz_term = jzzz_term + Jzzz(i,k,l)*recursive_kron( n, [i,k,l], [sigmaZ,sigmaZ,sigmaZ] );
                 end
-                if length(Jxxx) > 1
+                if length(Jxxx) > 1 && Jxxx(i,k,l) ~= 0
                     jxxx_term = jxxx_term + Jxxx(i,k,l)*recursive_kron( n, [i,k,l], [sigmaX,sigmaX,sigmaX] );
                 end
             end 
