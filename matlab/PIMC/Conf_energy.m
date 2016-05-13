1b360091b896d691e7950737c78bc4b62ecbf457
David Wise <wisedavid@mac.com>
2016-05-13 16:30:18 +0100

    Refactored energy calculator to use only one nested loop

--- matlab/PIMC/Conf_energy.m
+++ matlab/PIMC/Conf_energy.m
@@ -10,42 +10,50 @@ function [ out ] = Conf_energy(spin_conf, HParams)
     Jzzz_tot = 0
     Jxxx_tot = 0
     
-    % Find h energy terms
-    for i = 1:length(h)
-        h_tot = h_tot + h(i)*spin_conf(i)
-    end
-    
-    % Find Jzz en terms
-    for i = 1:length(Jzz)
-        for j = i:length(Jzz)
-            Jzz_tot = Jzz_tot + Jzz(i,j)*spin_conf(i)*spin_conf(j)
-        end
-    end
-    
-    % Find Jxx en terms
-    for i = 1:length(Jxx)
-        for j = i:length(Jxx)
-            Jxx_tot = Jxx_tot + Jxx(i,j)*spin_conf(i)*spin_conf(j)
-        end
-    end
+%     % Find h energy terms
+%     for i = 1:length(h)
+%         
+%     end
+%     
+%     % Find Jzz en terms
+%     for i = 1:length(Jzz)
+%         for j = i+1:length(Jzz)
+%             
+%         end
+%     end
+%     
+%     % Find Jxx en terms
+%     for i = 1:length(Jxx)
+%         for j = i+1:length(Jxx)
+%             
+%         end
+%     end
     
     % Find Jzzz en terms
-    for i = 1:length(Jzzz)
-        for j = i:length(Jzzz)
-            for k = j:length(Jzzz)
-                Jzzz_tot = Jzzz_tot + Jzzz(i,j,k)*spin_conf(i)*spin_conf(j)*spin_conf(k)
+    for i = 1:n
+        if length(h) > 1        
+            h_tot = h_tot + h(i)*spin_conf(i)
+        end
+        for j = i+1:n
+            if length(Jzz) > 1
+                Jzz_tot = Jzz_tot + Jzz(i,j)*spin_conf(i)*spin_conf(j)
+            end           
+            if length(Jxx) > 1
+                Jxx_tot = Jxx_tot + Jxx(i,j)*spin_conf(i)*spin_conf(j)
+            end
+            for k = j+1:n
+                if length(Jzzz) > 1
+                    Jzzz_tot = Jzzz_tot + Jzzz(i,j,k)*spin_conf(i)*spin_conf(j)*spin_conf(k)
+                end
+                if length(Jxxx) > 1
+                    Jxxx_tot = Jxxx_tot + Jxxx(i,j,k)*spin_conf(i)*spin_conf(j)*spin_conf(k)
+                end
             end
         end
     end
     
-    % Find Jxxx en terms
-    for i = 1:length(Jxxx)
-        for j = i:length(Jxxx)
-            for k = j:length(Jxxx)
-                Jxxx_tot = Jxxx_tot + Jxxx(i,j,k)*spin_conf(i)*spin_conf(j)*spin_conf(k)
-            end
-        end
-    end
+    
+ 
     
     out = h_tot + Jzz_tot + Jxx_tot + Jzzz_tot + Jxxx_tot
         
