585149b16c2f39d33915abcd18fcbbf88a542e0f
David Wise <wisedavid@mac.com>
2016-05-19 14:47:21 +0100

    Changed piqmc so that it no longer uses conf_energy, instead calculates energy change only for relevant spins, this makes it faster

--- matlab/PIMC/piqmc.m
+++ matlab/PIMC/piqmc.m
@@ -5,18 +5,19 @@ function [ conf, energy ] = piqmc(spin_start, HParams, monte_steps, trotter_slic
     start_spin_config = repmat(transpose(spin_start), trotter_slices, 1);
     
     % Calculate the initial energy
-    start_eng = Ham_d1(start_spin_config, HParams, trotter_slices, Temperature, G_start);
+    %start_eng = Ham_d1(start_spin_config, HParams, trotter_slices, Temperature, G_start);
     
     % Create updated variables of instantaneous spin config and total
     % energy
     spin_config = start_spin_config;
     
-    total_energy = start_eng;
+    %total_energy = start_eng;
     
     n = length(spin_start);
     
     G = G_start;
-    
+    P = trotter_slices
+    T = Temperature
     step_value = (G_start - (0.00000001))/monte_steps;
     
 %     energyFunction = buildEnergyFunction(h, Jzz, Jzzz)
@@ -31,39 +32,67 @@ function [ conf, energy ] = piqmc(spin_start, HParams, monte_steps, trotter_slic
         for i = 1:n;
             
             % Perform local flips and evals
-            total_energy
-            for j = 1:trotter_slices;
+            %total_energy;
+            J_orth = -(P*T/2)*log(tanh(G/(P*T)));
+            for slice = 1:trotter_slices;
                 % Create variables to store new energy and configuration
                 new_spin_config = spin_config;
-                
+                indices_to_flip = randperm(n, step_flips).';
                 %flip local spin
-                new_spin_config(j,:) = flip_spin(spin_config(j,:),step_flips);
-%                 %flip global spin
-%                 for h = 1:length(glob_flip_index)
-%                     new_spin_config(:,glob_flip_index(h)) = -new_spin_config(:,glob_flip_index(h));
-%                 end
-                new_total_energy = Ham_d1(new_spin_config, HParams, trotter_slices, Temperature, G);
-                p_t = tran_prob(new_total_energy, total_energy, trotter_slices, Temperature, n, G);
+                for flip = 1:length(indices_to_flip);
+                    new_spin_config(slice,indices_to_flip(flip)) = -new_spin_config(slice,indices_to_flip(flip));
+                end
+                ediff = 0;
+                for spin_index = 1:length(indices_to_flip);
+                    ediff = ediff+ -2.0*h(indices_to_flip(spin_index))*new_spin_config(slice,indices_to_flip(spin_index));
+                end
+                for spin_index = 1:length(indices_to_flip);
+                    for neib = 1:n;
+                        if neib ~= indices_to_flip(spin_index)
+                            ediff = ediff + -2.0*Jzz(indices_to_flip(spin_index), neib)*new_spin_config(slice,indices_to_flip(spin_index))*new_spin_config(slice,neib);
+                        end
+                    end
+                end
+                if slice == 1;
+                    left = trotter_slices;
+                    right = 2;
+                elseif slice == trotter_slices;
+                    left = trotter_slices - 1;
+                    right = 1;
+                else
+                    left == slice - 1;
+                    right = slice +1;
+                end
+                for spin_index = 1:length(indices_to_flip);
+                    ediff = ediff + -2.0*J_orth*new_spin_config(slice, indices_to_flip(spin_index))*new_spin_config(left, indices_to_flip(spin_index));
+                    ediff = ediff + -2.0*J_orth*new_spin_config(slice, indices_to_flip(spin_index))*new_spin_config(right, indices_to_flip(spin_index));
+                end
+                ediff;
+                
+                p_t = tran_prob(0,0,ediff, trotter_slices, Temperature, n, G);
                 x_1 = rand;
                 if x_1 <= p_t;
                     spin_config = new_spin_config;
-                    total_energy = new_total_energy;
+%                     total_energy = Ham_d1(spin_config, HParams, trotter_slices, Temperature, G);
                 end
+                ediff = 0;
+%            
+                
             end
-            glob_flip_index = randperm(n,step_flips);
-            % Perform global spin flips and evals
-            glob_spin_config = spin_config;
-            for j = 1:trotter_slices;
-                for k = 1:length(glob_flip_index);
-                    glob_spin_config(j,glob_flip_index(k)) = -glob_spin_config(j,glob_flip_index(k));
-                end
-            end
-            new_glob_energy = Ham_d1(glob_spin_config, HParams, trotter_slices, Temperature, G);
-            p_t_g = tran_prob(new_glob_energy, total_energy, trotter_slices, Temperature, n, G);
-            x_g = rand;
-            if x_g <= p_t_g;
-                spin_config = glob_spin_config;
-            end
+%             glob_flip_index = randperm(n,step_flips);
+%             % Perform global spin flips and evals
+%             glob_spin_config = spin_config;
+%             for slice = 1:trotter_slices;
+%                 for k = 1:length(glob_flip_index);
+%                     glob_spin_config(slice,glob_flip_index(k)) = -glob_spin_config(slice,glob_flip_index(k));
+%                 end
+%             end
+%             new_glob_energy = Ham_d1(glob_spin_config, HParams, trotter_slices, Temperature, G);
+%             p_t_g = tran_prob(new_glob_energy, total_energy,0, trotter_slices, Temperature, n, G);
+%             x_g = rand;
+%             if x_g <= p_t_g;
+%                 spin_config = glob_spin_config;
+%             end
         end
     end
 
