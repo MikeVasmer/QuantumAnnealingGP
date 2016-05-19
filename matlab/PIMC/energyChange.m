225070b5e952cf5ad44eac11fec1bf49e2c1a6ca
David Wise <wisedavid@mac.com>
2016-05-19 14:48:04 +0100

    Added energyChange function which performs the efficient energy change calculation for piqmc

---
+++ matlab/PIMC/energyChange.m
@@ -0,0 +1,49 @@
+function [ deltaH ] = energyChange(new_spin_config, indices_to_flip, trotter_slices, J_orth, slice, h, Jzz, Jzzz)
+
+    ediff = 0;
+
+    
+    for spin_index = 1:length(indices_to_flip);
+        ediff = ediff+ -2.0*h(indices_to_flip(spin_index))*new_spin_config(slice,indices_to_flip(spin_index));
+    end
+
+    if isempty(Jzzz)
+        for spin_index = 1:length(indices_to_flip);
+            for neib_1 = 1:n;
+                if neib_1 ~= indices_to_flip(spin_index)
+                    ediff = ediff + -2.0*Jzz(indices_to_flip(spin_index), neib_1)*new_spin_config(slice,indices_to_flip(spin_index))*new_spin_config(slice,neib_1);
+                end
+            end
+        end
+    elseif isempty(Jzz)
+        for spin_index = 1:length(indices_to_flip);
+            for neib_1 = 1:n;
+                for neib_2 = (neib_1+1):n;
+                    if neib_1 ~= indices_to_flip(spin_index)
+                        ediff = ediff + -2.0*Jzzz(indices_to_flip(spin_index), neib_1, neib_2)*new_spin_config(slice,indices_to_flip(spin_index))*new_spin_config(slice,neib_1)*new_spin_config(slice,neib_2);
+                    end
+                end
+            end
+        end
+    end
+    
+    if slice == 1;
+        left = trotter_slices;
+        right = 2;
+    elseif slice == trotter_slices;
+        left = trotter_slices - 1;
+        right = 1;
+    else
+        left == slice - 1;
+        right = slice +1;
+    end
+    for spin_index = 1:length(indices_to_flip);
+        ediff = ediff + -2.0*J_orth*new_spin_config(slice, indices_to_flip(spin_index))*new_spin_config(left, indices_to_flip(spin_index));
+        ediff = ediff + -2.0*J_orth*new_spin_config(slice, indices_to_flip(spin_index))*new_spin_config(right, indices_to_flip(spin_index));
+    end
+    
+    deltaH = ediff;
+end
+    
+        
+        
