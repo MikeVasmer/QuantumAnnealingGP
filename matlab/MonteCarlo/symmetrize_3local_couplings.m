e06f73e99bd5334adec21853024b378c4cdfcffb
Danial Dervovic <dannydervovic@hotmail.com>
2016-05-19 18:55:55 +0100

    lol

--- matlab/MonteCarlo/symmetrize_3local_couplings.m
+++ matlab/MonteCarlo/symmetrize_3local_couplings.m
@@ -1,30 +1,35 @@
-function [ Mout ] = symmetrize_2local_couplings( Jzz )
+function [ Mout ] = symmetrize_3local_couplings( Jzzz )
 %SYMMETRIZE_3LOCAL_COUPLINGS Sets diagonals to zero and keeps upper right
 %tetrahedron of nxnxn matrix
 
-sizeJzzz = size(Jzz);
-Mout = Jzz;
+sizeJzzz = size(Jzzz);
+Mout = Jzzz;
 
 % Pairwise terms to zero
 for i = 1:sizeJzzz(1)
     for j = 1:sizeJzzz(2)
-            if i == j 
-                Mout(i,j) = 0;
+        for k = 1:sizeJzzz(3)
+            if i == j || j == k || i == k
+                Mout(i,j,k) = 0;
             end
             
+        end
     end
 end
 
 % Permutation symmetry
 for i = 1:sizeJzzz(1)
     for j = 1:sizeJzzz(2)
-        
-            if Mout(i,j) ~= 0
-                Mout(j,i) = Mout(i,j);
-                
+        for k = 1:sizeJzzz(3)
+            if Mout(i,j,k) ~= 0
+                Mout(j,i,k) = Mout(i,j,k);
+                Mout(j,k,i) = Mout(i,j,k);
+                Mout(k,j,i) = Mout(i,j,k);
+                Mout(k,i,j) = Mout(i,j,k);
+                Mout(i,k,j) = Mout(i,j,k);
             end
             
-       
+        end
     end
 end
             
