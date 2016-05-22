dfefd04678fd1fd5f73fe5ec23d1bf5513c1dcb6
Danial Dervovic <dannydervovic@hotmail.com>
2016-05-22 16:03:03 +0100

    Hardness test with global timeout

--- matlab/MonteCarlo/Hardness.m
+++ matlab/MonteCarlo/Hardness.m
@@ -1,17 +1,57 @@
-function [ metric ] = Hardness( Hparams, gs_energy, epsilon, mySolver, timeout, num_runs)
+function [ metric ] = Hardness(spinConfig, Hparams, gs_energy, epsilon, mySolver, timeOut, num_runs)
 %HARDNESS Calculates hardness metric of some problem H.
 % Metric is either:
 %       - time to epsilon with flag
 %       - epsilon at timeout with flag
 
-tic
-switch mySolver
 
-    case 
+%% TEST
+
+    solved_energy = 0;
     
-end  
+    
+    function hardnessmetric = test_hardness
+        
+        time_elapsed = 0;
+        
+        % Run n times, recording best solution and time taken
+        for run = 1: num_runs
+            
+            tic    
+            local_time = 0;            
+            solution = Solver(spinConfig, Hparams, mySolver);
+            solution_energy = solution{1};
+            local_time = toc;
+             
+            if solution_energy < solved_energy
+                solved_energy = solution_energy;
+                time_elapsed = local_time;
+            end
+            
+        end       
+        
+        deficit = (solved_energy - gs_energy);
+        
+        if deficit < epsilon
 
-
+            hardnessmetric = {time_elapsed, 'TTS'};
+            
+        else
+            
+            hardnessmetric = {deficit, ['Accuracy acheived in ', num2str(time_elapsed), ' seconds.']};
+            
+        end
+        
+    end
+    
+ try 
+     metric = timeout(@test_hardness, (num_runs*timeOut));    
+ catch
+     metric = {(solved_energy - gs_energy), ['Timeout in ', num2str(timeOut*num_runs), ' seconds.']} ;
+ end
 
 end
+
+    
+
 
---
+++ matlab/MonteCarlo/testHardness.m
@@ -0,0 +1,36 @@
+clc
+clearvars
+
+n_qubits = 50;
+conn_density = 0.5;
+h_range = [-1, 1];
+J_range = [-1, 1];
+disorder = round(n_qubits / 2);
+beta = 10000;
+
+%Hparams = generate_random_3local_hamiltonian(n_qubits, conn_density, h_range, J_range);
+Hparams = {[], NN_couplings(n_qubits, 1), [], [], []};
+
+spinConfig = generate_spins(n_qubits, disorder);
+
+gs_energy = -8;
+epsilon = 0.001;
+timeOut = 0.1;
+num_runs = 3;
+
+%solutionMet = Solver(spinConfig, Hparams, 'Metropolis');
+%solutionHB = Solver(spinConfig, Hparams, 'HeatBath');
+%solutionSA = Solver(spinConfig, Hparams, 'SimulatedAnnealing');
+tic
+hardnessSA = Hardness(spinConfig, Hparams, gs_energy, epsilon, 'SimulatedAnnealing', timeOut, num_runs);
+%solutionPIQMC = Solver(spinConfig, Hparams, 'PIQMC');
+
+%disp(solutionMet{1});
+%disp(solutionHB{1});
+%disp(solutionSA{1});
+disp(hardnessSA{1});
+disp(hardnessSA{2});
+%disp(solutionPIQMC{1});
+toc
+
+
