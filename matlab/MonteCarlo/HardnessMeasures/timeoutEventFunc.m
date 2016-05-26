63c6662544fc799ef5acc52c699cde393b092417
Alex Morgan <axm108@gmail.com>
2016-05-26 12:54:10 +0100

    Update Conf_energy. Commented out the timeout function.

--- matlab/MonteCarlo/HardnessMeasures/timeoutEventFunc.m
+++ matlab/MonteCarlo/HardnessMeasures/timeoutEventFunc.m
@@ -1,7 +1,7 @@
 function timeoutEventFunc( hTimer, eventData )
 % Function to raise timeout flag
-        global timeoutFlag;
-        timeoutFlag = true;     
-        error('Warning: Woooo BROOO!! Time out brooo!');
+        %global timeoutFlag;
+        %timeoutFlag = true;     
+        %error('Warning: Woooo BROOO!! Time out brooo!');
 end
 
