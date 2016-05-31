a227f3a563ea1978efc201163ef7acba16e80d90
Alex Morgan <axm108@gmail.com>
2016-05-31 11:12:12 +0100

    3-local solver analysis

--- matlab/Analysis/testGetSolverTimes.m
+++ matlab/Analysis/testGetSolverTimes.m
@@ -4,5 +4,5 @@
 % getSolverTimes('C:\Users\dwise\UCL\QuantumAnnealingGP\matlab\loop-adaptive-optimisation\files\3Local\NN', 100, 15, 0.95)
 % getSolverTimes('C:\Users\dwise\UCL\QuantumAnnealingGP\matlab\loop-adaptive-optimisation\files\2Local\30Qubits', 50, 10, 0.95)
 
-percGetSolverTimes('C:\Users\dwise\UCL\QuantumAnnealingGP\matlab\loop-adaptive-optimisation\files\3Local\NN\81Qubits\David', 100, 0.9)
+percGetSolverTimes('C:\Users\amorgan\Documents\MATLAB\quantum-annealing\QuantumAnnealingGP\matlab\loop-adaptive-optimisation\files\3Local\NN\81Qubits\Alex', 40, 0.9)
 
