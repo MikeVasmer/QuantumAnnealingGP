ce7d781dfe997ab2f32386ea892846063885cdc0
unknown <dwise@qcdt10.lcn.ucl.ac.uk>
2016-06-04 14:45:07 +0100

    All commits

--- matlab/Analysis/testGetSolverTimes.m
+++ matlab/Analysis/testGetSolverTimes.m
@@ -5,7 +5,7 @@
 % getSolverTimes('C:\Users\dwise\UCL\QuantumAnnealingGP\matlab\loop-adaptive-optimisation\files\2Local\30Qubits', 50, 10, 0.95)
 
 
-percGetSolverTimes('C:\Users\dwise\UCL\QuantumAnnealingGP\matlab\loop-adaptive-optimisation\files\3Local\NN\81Qubits\David', 1, 0.9)
+percGetSolverTimes('C:\Users\dwise\UCL\QuantumAnnealingGP\matlab\loop-adaptive-optimisation\files\2Local\NN\28May', 40, 0.9)
 
 
 % percGetSolverTimes('C:\Users\ddervovic\Documents\QuantumAnnealingGP\matlab\loop-adaptive-optimisation\files\3Local\NN\81Qubits\Dan', 40, 0.9)
