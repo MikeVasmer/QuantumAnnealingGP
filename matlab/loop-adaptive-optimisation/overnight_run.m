1d705cf6684d3ce9cc2bfe3bad96fd446d056967
Mike Vasmer <michael_vasmer@hotmail.co.uk>
2016-06-05 11:15:33 +0100

    Chimera Runs Mike

--- matlab/loop-adaptive-optimisation/overnight_run.m
+++ matlab/loop-adaptive-optimisation/overnight_run.m
@@ -1,3 +1,3 @@
 runLao_loop
 
-percGetSolverTimes('C:\Users\amorgan\Documents\MATLAB\quantum-annealing\QuantumAnnealingGP\matlab\loop-adaptive-optimisation\files\2Local\Chimera', 40, 0.9)
+percGetSolverTimes('C:\Users\mvasmer\MRes\QuantumAnnealingGP\matlab\loop-adaptive-optimisation\files\2Local\Chimera', 40, 0.9)
