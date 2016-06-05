75096371ecbf14094e8e045456fd0a0d76763a5d
David Wise <wisedavid@mac.com>
2016-06-05 11:06:48 +0100

    Minor changes for graph construction

--- matlab/Analysis/densityTest.m
+++ matlab/Analysis/densityTest.m
@@ -23,30 +23,44 @@
 percSArand = zeros(length(percSA),1);
 percPIQMCrand = zeros(length(percPIQMC),1);
 percHBrand = zeros(length(percHB),1);
+percSA(end) = 1;
+percPIQMC(end) = 0.5
 
 for i = 1:length(percSA);
     x = 1.025 - rand*0.03;
     y = 1.025 - rand*((1/percSA(i))*0.02);
     percSArand(i) = percSA(i)*(y^2);
     percPIQMCrand(i) = percPIQMC(i)*(x^2);
+    
     percHBrand(i) = percHB(i)*y;
+%     percSArand(i) = percSArand(i)/(max(percSArand));
+%     percPIQMCrand(i) = percPIQMCrand(i)/(max(percPIQMCrand));
 end
     
+percSA23 = cat(1, percSA(1:300), percSA3);
+percPIQMC23 = cat(1, percPIQMC(1:300), percPIQMC3);
 
-scatter(percPIQMCrand(:), percSArand(:))
+scatter(percPIQMC23(:), percSA23(:))
+lims = [0 1 0 1];
 
-dat = [percPIQMCrand(:), percSArand(:)];
-figure
-n = hist3(dat, [50 50])
+percPIQMC23(end+1) = 0.2;
+percSA23(end+1) = 1;
+axis(lims);
+dat = [percPIQMC23(:), percSA23(:)];
+figure;
+x = 0:100;
+y = x
+n = hist3(dat, [20 20])
+
 
 n1 = n';
 n1(size(n,1) + 1, size(n,2) + 1) = 0;
 
-% xb = linspace(min(dat(:,1)),max(dat(:,1)),size(n,1)+1);
-% yb = linspace(min(dat(:,2)),max(dat(:,2)),size(n,1)+1);
+xb = linspace(min(dat(:,1)),max(dat(:,1)),size(n,1)+1);
+yb = linspace(min(dat(:,2)),max(dat(:,2)),size(n,1)+1);
 
-xb = linspace(0,1, size(n,1)+1);
-yb = linspace(0,1, size(n,1)+1);
+% xb = linspace(0,1, size(n,1)+1);
+% yb = linspace(0,1, size(n,1)+1);
 
 
 h = pcolor(xb,yb,n1);
@@ -54,7 +68,7 @@ set(h, 'EdgeColor', 'none');
 
 % h.ZData = ones(size(n1)) * -max(max(n));
 colormap(flipud(colormap(jet))) % heat map
-colormap(jet)
+% colormap(jet)
 title('PIQMC vs SA');
 grid on
 view(2);
