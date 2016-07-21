b3b886f5838599d98efac3451b899b0b6cf96014
David Wise <wisedavid@mac.com>
2016-07-21 17:00:00 +0100

    Minor changes

--- matlab/MonteCarlo/testSolver.m
+++ matlab/MonteCarlo/testSolver.m
@@ -4,15 +4,15 @@ data = importdata('config.txt',',',1);
 data = data.data;
 
 
-n_qubits = data(1);
+n_qubits = 81;
 conn_density = data(2);
 h_range = [0, 0];
 J_range = [data(5), data(6)];
 disorder = round(n_qubits / 2);
 
 % Hparams = generate_random_3local_hamiltonian(n_qubits, conn_density, h_range, J_range);
-% % Hparams = generate_random_2local_hamiltonian(n_qubits, conn_density, h_range, J_range);
-Hparams = {0, NN_couplings(n_qubits, 1), 0, 0, 0};
+Hparams = generate_random_2local_hamiltonian(n_qubits, conn_density, h_range, J_range);
+% Hparams = {0, NN_couplings(n_qubits, 1), 0, 0, 0};
 Hparams{1} = 0
 spinConfig = generate_spins(n_qubits, disorder);
 
